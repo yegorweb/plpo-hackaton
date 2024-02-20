16af66d8ca47fc192db5e2a33ecba19c1d1ce426
regtyit <titov54275@gmail.com>
2024-02-20 18:43:23 +0400

    dont merge

--- src/components/AboutPLPO.vue
+++ src/components/AboutPLPO.vue
@@ -1,31 +1,51 @@
 <script setup>
+gsap.from(".sponsor", {
+  duration: 2,
+  scale: 0.5, 
+  opacity: 0, 
+  delay: 0.5, 
+  stagger: 0.2,
+  ease: "elastic", 
+});
+
+document.querySelectorAll(".sponsor").forEach(function(sponsor) {
+    sponsor.addEventListener("click", function() {
+    gsap.to(".sponsor", {
+      duration: 0.5, 
+      opacity: 0, 
+      y: -100, 
+      stagger: 0.2,
+      ease: "back.in"
+    });
+  });
+});
 </script>
 <template>
     <div class="plpo">
         <h1 class="text-center title">Спонсоры</h1>
         <div style="height: 25dvh; background:#EAEAC3; overflow: scroll;" class="d-flex align-center justify-md-center">
             
-            <div>
+            <div class="sponsor">
                 <img src="../assets/bastion.jpg" alt="">
             </div>
             
            
-            <div>
+            <div class="sponsor">
                 <img src="../assets/bk.png" alt="">
             </div>
-            <div>
+            <div class="sponsor">
                 <img src="../assets/plpo.png" alt="">
             </div>
-            <div>
+            <div class="sponsor">
                 <img src="../assets/qbit.png" alt="">
             </div>
-            <div>
+            <div class="sponsor">
                 <img src="../assets/giv.png" alt="">
             </div>
-            <div>
+            <div class="sponsor">
                 <img src="../assets/trik.png" alt="">
             </div>
-            <div>
+            <div class="sponsor">
                 <img src="../assets/pro.png" alt="">
             </div>
 
